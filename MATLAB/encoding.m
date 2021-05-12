98cccc45490cb7ab90f12fc3dc7a3a1d1e59c73d
Jia Lu <jia.lu.angela@gmail.com>
2021-05-12 11:56:48 -0400

    Add files via upload

--- MATLAB/encoding.m
+++ MATLAB/encoding.m
@@ -1,5 +1,5 @@
 function encoding(taskID,path,r0,spacing, seeding_noise, kernel_noise, ...
-    plate_shape, text_bit, num_replicate, IFCROP)
+    plate_shape, scale, text_bit, num_replicate, IFCROP)
 
 % Paths
 path_text    = [path 'plaintext/'];
@@ -26,8 +26,13 @@ negativeExponent  = postitiveExponent; % h2
 plateRadius       = negativeRadius;
 plateExponent     = 2000;
 pixelSize         = 200;% resolution, um per pixel
-plateDiameter     = pixelSize * 450; % Petri dish diameter, um
 
+if IFCROP == 0
+    plateDiameter = pixelSize * 450;% Petri dish diameter, um
+elseif IFCROP == 1
+    plateDiameter = pixelSize * 790;
+end
+nt = 3000;
 %% -------------------------------------------------------------
 % GROWTH DOMAIN
 
@@ -42,7 +47,9 @@ end
 x = ((0:(gridSize - 1)) - (gridSize/2)) + 0.5;
 y = x;
 [x, y] = meshgrid(x,y);
-plate = get_Plate(plate_shape,pixelSize,plateDiameter,x,y,1);
+plate = get_Plate(plate_shape,x,y,scale);
+figname = strcat(path_initial, 'Shape_ID_', num2str(taskID),'.jpg');
+imwrite(plate,figname,'jpg');
 
 % Get distance of plate center to edge
 r = sqrt(x.^2 + y.^2)*pixelSize;
@@ -50,8 +57,8 @@ distanceToEdge = get_distanceToEdge(plate);
 
 %% -------------------------------------------------------------
 % PLAINTEXT
-plaintext = get_Plaintext(taskID, num_replicate); % decimal
-plaintext_binary = plaintext2binary_bybit(plaintext,text_bit);
+plaintext = get_Plaintext(taskID, num_replicate); 
+plaintext_binary = plaintext_de2bi(plaintext,text_bit);
 
 % Save plaintext
 textname = strcat(path_text,'Plaintext_ID_', num2str(taskID),'.txt');
@@ -62,6 +69,7 @@ fclose(fileID);
 %% -------------------------------------------------------------
 % INITIAL PATTERN
 center = get_SeedingCenter(plaintext_binary, spacing);
+% center = [0 0]
 
 % Configuration, 0-> no seeding, 1-> spot seeding
 config = get_meshelements(x, y, center, r0);
@@ -69,9 +77,14 @@ config = get_meshelements(x, y, center, r0);
 % Add noise
 noise_length = sum(sum(config));
 values = get_Noise(seeding_noise,noise_length);
-
-% Initiate colony
 colony = zeros(gridSize);
+
+% Plot seeding configuration
+colony_copy = plate + colony;
+colony_copy(config == 1) = 1;
+figname = strcat(path_initial, 'Config_ID_', num2str(taskID),'.jpg');
+imwrite(colony_copy,figname,'jpg');
+
 colony(config == 1) = values;
 
 % Plot initial pattern
@@ -85,7 +98,7 @@ colonyTracks = colony;
 iterationResidual = nan(nt, 1);
 
 figure
-for i = 1:3000,
+for i = 1:nt
     colonyOld = colonyTracks;
     filterKernel =...
         makeKernel(plateDiameter, gridSize, positiveRadius,...
@@ -108,9 +121,11 @@ for i = 1:3000,
     
     % Plot colony
     IM = colonyTracks*0 + colony + plate*5;
-%     if mod(i,10) == 0
-%         imshow(IM)
-%     end
+    if mod(i,10) == 0
+        imshow(IM)
+        figname = strcat(path_final,int2str(taskID),'_',int2str(i),'.jpg');
+        imwrite(IM, figname, 'jpg');
+    end
     
     % Terminate the simulation if colony stops growing
     iterationResidual(i) = length(find(colonyOld ~= colonyTracks));
@@ -126,14 +141,19 @@ imwrite(IM, figname2, 'jpg');
 
 % Save cropped pattern center
 if IFCROP == 1
-    cropped_Img  = crop_Img(IM,x,y);
-    a = (plateDiameter/pixelSize-450)/2;
-    b = a + 450;
+    cropped_Img = crop_Img(IM,x,y,scale,0); 
+    cropped_Img_copy = crop_Img(IM,x,y,scale,1);    
+    a = (plateDiameter/pixelSize-450/scale)/2;
+    b = a + 450/scale;
     cropped_Img = cropped_Img(a:b,a:b);
+    cropped_Img_copy = cropped_Img_copy(a:b,a:b);
+    size(cropped_Img)
     
-    figure
-    imshow(cropped_Img);
-    figname3 = strcat(path_cropped, 'Cropped_Img_ID_', num2str(taskID),'.jpg');
+    figure;imshow(cropped_Img);
+    figname3 = strcat(path_cropped, 'Cropped_ID_', num2str(taskID),'.jpg');
     imwrite(cropped_Img, figname3, 'jpg');
+    figname4 = strcat(path_cropped, 'Cropped_WBG_ID_', num2str(taskID),'.jpg');
+    imwrite(cropped_Img_copy, figname4, 'jpg');
+
 end
 end
--- MATLAB/main.m
+++ MATLAB/main.m
@@ -9,19 +9,17 @@ path = [pwd '/']; % root directory
 % -------------------------------------------------------------------------
 %    
 %   SEEDING NOISE
-%       Gaussian distribution, mean 0.5, deviation:
-%       1 -- 0.5      2 -- 0.25      3 -- 0.1      4 -- no noise
+%       1: Uniform distribution;              2: Gaussian distribution,large var;
+%       3: Gaussian distribution, small var;  4: no noise
 %
 %   GROWTH KERNEL NOISE
-%       1: no white noise;       2: low white noise;
+%       1: no noise;             2: low white noise;
 %       3: medium white noise;   4: high white noise
 %
 %   PLATE SHAPE
-%      Default -- 
-%               11: circular plate
-%      Encryption --
-%               1: circle;  2:square;   3:rectangle;    
-%               4:diamond;  5:triangle 
+%       Default --    11: circule
+%       Encryption -- 1: circle;  2:square; 3:diamond;  4:triangle
+%       scale: shrink by scale
 % 
 %   TEXT LENGTH
 %       2, 4, 6 or 8 bits
@@ -31,33 +29,41 @@ path = [pwd '/']; % root directory
 %
 % -------------------------------------------------------------------------
 
-r0 = 5; % radius of seeding configuration
-spacing = 25; % spot spacing
+r0 = 15; % radius of seeding configuration, 5
+spacing = 50; % spot spacing, 15
 seeding_noise = 2;
 kernel_noise = 1;
-plate_shape = 11;
+plate_shape = 1;
+scale = 1; % <=1
 text_bit = 4; 
 num_replicate = 1000; 
-IFCROP = 0; 
 
-
-% SIMULATION
-for i = 1: (2^text_bit-1)
-    for j = 1:num_replicate
-        id = (i - 1) * num_replicate + j;
-        
-        if id == 1 % Write simulation details in txt file 
-            filename = strcat(path,'details.txt');
-            fileID = fopen(filename,'w');
-            formatSpec = 'r0:%i\nspacing:%i\nseeding_noise:%i\nkernel_noise:%i\nplate_shape:%i\ntext_bit:%i\nnum_replicate:%i\nIFCROP:%i\n';;
-            fprintf(fileID,formatSpec,r0,spacing,seeding_noise,kernel_noise,plate_shape,text_bit,num_replicate,IFCROP);
-            fclose(fileID);
-        end
-
-        encoding(id, path, r0, spacing, seeding_noise, kernel_noise, ...
-            plate_shape, text_bit, num_replicate, IFCROP);
-    end 
+if plate_shape ~= 11
+    IFCROP = 1; 
+else 
+    IFCROP = 0;
 end
+
+% SIMULATION 
+% for i = 1:1%(2^text_bit-1)
+%     for j = 1:num_replicate
+%         id = (i - 1) * num_replicate + j;
+%         encoding(id, path, r0, spacing, seeding_noise, kernel_noise, ...
+%             plate_shape, scale, text_bit, num_replicate, IFCROP);
+%     end 
+% end
+
+for i = 1:4
+    encoding(i, path, r0, spacing, seeding_noise, kernel_noise, ...
+             i, scale, text_bit, num_replicate, IFCROP);
+end
+
+% Save parameters
+filename = strcat(path,'Simulation_Parameters.txt');
+fileID = fopen(filename,'w');
+fprintf(fileID, 'r0: %i\nspacing: %i\nseeding: %i\nkernel: %i\nshape: %i\nscale: %f\nbit: %i\ncrop: %i',...
+    r0,spacing,seeding_noise,kernel_noise,plate_shape,scale,text_bit,IFCROP);
+fclose(fileID);
 
 disp('DONE')
 
